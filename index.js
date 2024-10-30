0a2ab19e9f1c611058d47b3aad12d29f35494ba1
Leo Bermeo Kittyle <leobermeokittyle@gmail.com>
2024-10-30 11:40:24 +0100

    Running fourth line of code

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ let ten = 10;
 let five = 5;
 let one = 1;
 let two = 2;
+let text1 = '1';
+let text2 = '2';
 
 console.log(`¿El número ${ten} es mayor que el número ${five}?`, ten > five);
 console.log(`¿El número ${ten} es menor que el número ${five}?`, ten < five);
 console.log(`¿El número ${one} es igual que el número ${two}?`, one == two);
+console.log(`¿El texto ${text1} es igual al texto ${text2}?`, one == two);
