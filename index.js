9406e5f05316e8b8217beaf7765d27c51c68d50b
Leo Bermeo Kittyle <leobermeokittyle@gmail.com>
2024-11-02 17:09:33 +0100

    Running eight and nineth lines of code

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let two = 2;
 let text1 = '1';
 let text2 = '2';
 let booleano = false;
+let three = 3;
 
 console.log(`¿El número ${ten} es mayor que el número ${five}?`, ten > five);
 console.log(`¿El número ${ten} es menor que el número ${five}?`, ten < five);
@@ -15,3 +16,5 @@ console.log(`¿El texto ${text1} es igual al texto ${text2}?`, one == two);
 console.log("¿Los objetos son iguales?", booleano);
 console.log("AND lógico:", booleano);
 console.log("AND lógico:", !booleano);
+console.log(`La suma de ${ten} + ${three}:`, ten + three);
+console.log(`La resta de ${ten} - ${three}:`, ten - three);
